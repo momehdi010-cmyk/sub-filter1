f47df120cde0410dcac270a79d6daefa1f0b20e9
momehdi010-cmyk <momehdi010@gmail.com>
2026-07-07 19:36:27 +0330

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,9 +1,18 @@
 import base64
 import requests
+import time
 
 SUB_URL = "https://zeus-panel-edqrba.zeus-ikyheb.workers.dev/feed/Deiii"
 
-r = requests.get(SUB_URL)
+headers = {
+    "Cache-Control": "no-cache",
+    "Pragma": "no-cache",
+    "User-Agent": "Mozilla/5.0"
+}
+
+url = f"{SUB_URL}?t={int(time.time())}"
+
+r = requests.get(url, headers=headers)
 r.raise_for_status()
 
 decoded = base64.b64decode(r.text.strip()).decode("utf-8")
@@ -15,9 +24,7 @@ filtered = [
     if "@0.0.0.0:1" not in line
 ]
 
-encoded = base64.b64encode(
-    "\n".join(filtered).encode()
-).decode()
+encoded = base64.b64encode("\n".join(filtered).encode()).decode()
 
 with open("sub.txt", "w") as f:
     f.write(encoded)
